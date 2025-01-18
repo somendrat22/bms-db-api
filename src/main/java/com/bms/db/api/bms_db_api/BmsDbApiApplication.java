e9431f0cf828bd0de5465cda7179ea38aa4354be
Somendrat22 <somendrat22@gmail.com>
2025-01-18 12:45:10 +0530

    DB: Added initial code

---
+++ src/main/java/com/bms/db/api/bms_db_api/BmsDbApiApplication.java
@@ -0,0 +1,13 @@
+package com.bms.db.api.bms_db_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmsDbApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BmsDbApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bms/db/api/bms_db_api/BmsDbApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bms.db.api.bms_db_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BmsDbApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
