068e658aec4790685d45ad096e090ef53652f061
Somendrat22 <somendrat22@gmail.com>
2025-01-19 13:03:38 +0530

    DB: App user class lombok removed

--- src/main/java/com/bms/db/api/bms_db_api/models/AppUser.java
+++ src/main/java/com/bms/db/api/bms_db_api/models/AppUser.java
@@ -1,12 +1,12 @@
 package com.bms.db.api.bms_db_api.models;
 
 import jakarta.persistence.*;
-import lombok.Data;
+import lombok.*;
 
 import java.util.UUID;
 
 @Entity
-@Data
+
 public class AppUser {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -20,4 +20,73 @@ public class AppUser {
     String userType;
     @Column(unique = true, nullable = false)
     Long contactNumber;
+
+    public AppUser(UUID id, String firstName, String lastName, String email, String password, String userType, Long contactNumber) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.userType = userType;
+        this.contactNumber = contactNumber;
+    }
+
+    public AppUser() {
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUserType() {
+        return userType;
+    }
+
+    public void setUserType(String userType) {
+        this.userType = userType;
+    }
+
+    public Long getContactNumber() {
+        return contactNumber;
+    }
+
+    public void setContactNumber(Long contactNumber) {
+        this.contactNumber = contactNumber;
+    }
 }
